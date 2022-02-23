ec5f5d2aabde761c3a9ea0f7400d4cdbe94e84c2
Yejun Kim <unsigndid@gmail.com>
2022-02-23 12:36:23 +0900

    decorate modal

--- src/components/system/popcontent.tsx
+++ src/components/system/popcontent.tsx
@@ -138,7 +138,42 @@ export function PopContent({
                     },
                   }}
                 >
-                  {content.text}
+                  <div
+                    className="header"
+                    style={{ backgroundColor: CurrentTheme.panelColor }}
+                  >
+                    <p style={{ color: CurrentTheme.textSubBlackColor }}>
+                      Rename Window
+                    </p>
+                    <div
+                      title="Cancel"
+                      onClick={() => {
+                        setEditModal(null);
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="27"
+                        height="27"
+                        fill={CurrentTheme.shortcutIconColor}
+                        className="bi bi-x"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="body">
+                    <i style={{ fontSize: '14px' }}>
+                      네 모습 그대로 미움 받는 것이 너 아닌 다른 모습으로 사랑
+                      받는 것보다 낫다.
+                    </i>
+                    <br />
+                    -앙드레 지드-
+                    <br />
+                    <br />
+                    고로 저희는 변수의 이름 바꾸기를 지원하지 않고 있습니다.
+                  </div>
                 </Modal>
               </div>
             )}

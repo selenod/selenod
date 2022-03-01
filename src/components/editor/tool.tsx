ddf58850797b22c7bda63bba6b3548983346f226
Yejun Kim <unsigndid@gmail.com>
2022-03-01 10:34:20 +0900

    make minor func

--- src/components/editor/tool.tsx
+++ src/components/editor/tool.tsx
@@ -104,6 +104,7 @@ export default function Tool() {
                 <div
                   style={{
                     backgroundColor: 'var(--panelPathColor)',
+                    maxWidth: '296px',
                   }}
                   title="Manage Windows"
                   onClick={() => {
@@ -117,24 +118,38 @@ export default function Tool() {
                     });
                   }}
                 >
-                  <p style={{ color: 'var(--textGrayColor)' }}>
+                  <p
+                    style={{
+                      display: 'block',
+                      color: 'var(--textGrayColor)',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                      paddingRight: '21px',
+                    }}
+                  >
                     MyPorject/
                     {
                       windowList.filter(
                         (window) => currentWindow === window.id
                       )[0].name
                     }
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-caret-down-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                    </svg>
                   </p>
+                  <svg
+                    style={{
+                      float: 'right',
+                      color: 'var(--textGrayColor)',
+                      marginTop: '-7.5px',
+                    }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bi bi-caret-down-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                  </svg>
                 </div>
               </Popover>
             </nav>
